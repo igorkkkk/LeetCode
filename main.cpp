58a43142a9be4971ad20243482adc80bcc077353
Igor <igoranchikkuzmin@yandex.ru>
2022-09-05 22:57:25 +0300

    LCA test v0.1
    Lowest Common Ancestor of a Binary Search Tree

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
-struct ListNode {
+
+
+struct TreeNode {
     int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-    };
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        return isPalindromeRecurse(head,head);
-    }
-    bool isPalindromeRecurse(ListNode*& head, ListNode* comp){
-        if(!comp){
-            return true;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if((root->val>p->val)&& (root->val> q->val)){
+            return lowestCommonAncestor(root->right,p,q);
         }
-        bool pal = isPalindromeRecurse(head, comp->next);
-
-        if(head->val != comp->val){
-            return false;
+        if(root->val<p->val&& root->val< q->val){
+            return lowestCommonAncestor(root->left,p,q);
         }
-        else {
-        head = head->next;
-        return pal;
+        return root;
     }
-
 };
 
 int main(int argc, const char * argv[]) {
     Solution t;
+    TreeNode root_one = TreeNode(6);
+    TreeNode root_two = TreeNode(2);
+    TreeNode root_three = TreeNode(0);
+    TreeNode root_four = TreeNode(4);
+    TreeNode root_five = TreeNode(3);
+    TreeNode root_six = TreeNode(5);
+    TreeNode root_seven = TreeNode(8);
+    TreeNode root_eight = TreeNode(7);
+    TreeNode root_nine = TreeNode(9);
 
-    ListNode one =  ListNode(1);
-    ListNode two = ListNode(2);
-    ListNode three = ListNode(3);
-    ListNode four = ListNode(2);
-    ListNode five = ListNode(1);
-
-
-    five.next = NULL;
-    one.next = &two;
-    two.next = &three;
-    three.next = &four;
-    four.next = &five;
-    ListNode* temp = &one;
-    std::cout <<t.isPalindrome(temp);
+    root_one.left = &root_two;
+    root_two.left = &root_three;
+    root_two.right = &root_four;
+    root_four.left = &root_five;
+    root_four.right = &root_six;
+    root_one.right = &root_seven;
+    root_seven.left = &root_eight;
+    root_seven.right = &root_nine;
+    TreeNode* ptr_root=  &root_one;
+    std::cout <<t.lowestCommonAncestor(ptr_root,&root_two,&root_four)->val;
     return 0;
 }
