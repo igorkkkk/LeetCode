005f924a89e5d0305d40ad5f7f4d6a39cfbfa8b3
Igor <igoranchikkuzmin@yandex.ru>
2022-09-07 21:52:51 +0300

    Middle of the Linked List

--- main.cpp
+++ main.cpp
@@ -8,37 +8,41 @@
 
 
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
-    std::vector<std::string> fizzBuzz(int n) {
-        std::vector<std::string> mass;
-        for(int i = 1;  i <=n; i++){
-
-            if(i%3 == 0){
-                mass.push_back("Fizz");
-                if(i%5 == 0){
-                    mass.back()+="Buzz";
-                }
-            }
-            else if(i%5 == 0){
-                mass.push_back("Buzz");
-            }
-            else{
-                mass.push_back(std::to_string(i));
-            }
-
+    ListNode* middleNode(ListNode* head) {
+        return middleNoteRecurse(head,head);
+    }
+    ListNode* middleNoteRecurse(ListNode*& middle,ListNode*& last){
+        if(last&&last->next){
+            return middleNoteRecurse(middle->next,last->next->next);
         }
-        return std::move(mass);
+        return middle;
     }
 };
 
 int main(int argc, const char * argv[]) {
     Solution t;
-    auto start = std::chrono::high_resolution_clock::now();
-    std::vector<std::string> v = t.fizzBuzz(15);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-    std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>(std::cout, " "));
-    std::cout<<"\n"<<duration.count();
+    ListNode one =  ListNode(1);
+    ListNode two = ListNode(2);
+    ListNode three = ListNode(3);
+    ListNode four = ListNode(2);
+    ListNode five = ListNode(1);
+
+
+    five.next = NULL;
+    one.next = &two;
+    two.next = &three;
+    three.next = &four;
+    four.next = &five;
+    ListNode* temp = &one;
+    std::cout<<"\n"<<t.middleNode(temp)->val;
     return 0;
 }
