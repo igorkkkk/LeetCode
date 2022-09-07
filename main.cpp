a9042ab9223317e129c79ccaf6fed97a592f05d7
Igor <igoranchikkuzmin@yandex.ru>
2022-09-07 12:13:51 +0300

    Ransom Note

--- main.cpp
+++ main.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
+#include <unordered_set>
 
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode*& rt, TreeNode* p, TreeNode* q) {
-
-        if((rt->val-p->val)*(rt->val-q->val)<=0) return rt;
-        else if( rt->val<p->val&& rt->val< q->val) rt = rt->right;
-        else rt = rt->left;
-        return lowestCommonAncestor(rt,p,q);
+    bool canConstruct(std::string ransomNote, std::string magazine) {
+        std::size_t tmp = magazine.find(ransomNote[0]);
+        if (tmp != std::string::npos) {
+            return canConstruct(ransomNote.erase(0,1), magazine.erase(tmp,1));
+        }
+        else {
+            return !ransomNote.length();
+        }
     }
 };
 
 int main(int argc, const char * argv[]) {
     Solution t;
-    TreeNode root_one = TreeNode(6);
-    TreeNode root_two = TreeNode(2);
-    TreeNode root_three = TreeNode(0);
-    TreeNode root_four = TreeNode(4);
-    TreeNode root_five = TreeNode(3);
-    TreeNode root_six = TreeNode(5);
-    TreeNode root_seven = TreeNode(8);
-    TreeNode root_eight = TreeNode(7);
-    TreeNode root_nine = TreeNode(9);
 
-    root_one.left = &root_two;
-    root_two.left = &root_three;
-    root_two.right = &root_four;
-    root_four.left = &root_five;
-    root_four.right = &root_six;
-    root_one.right = &root_seven;
-    root_seven.left = &root_eight;
-    root_seven.right = &root_nine;
-
-    TreeNode* ptr = &root_one;
-    std::cout <<t.lowestCommonAncestor(ptr,&root_two,&root_four)->val;
+    std::string ransomNote = "aa";
+    std::string magazine = "aab";
+    std::cout <<t.canConstruct(ransomNote,magazine);
     return 0;
 }
