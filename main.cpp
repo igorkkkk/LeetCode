5489ec0078892f551d7a8085d8f3e8347278c47a
Igor <igoranchikkuzmin@yandex.ru>
2022-09-20 17:14:42 +0300

    Number of Steps to Reduce a Number to Zero

--- main.cpp
+++ main.cpp
@@ -1,35 +1,19 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <iterator>
-#include <numeric>
-
-
 
 class Solution {
 public:
-    std::vector<int> kWeakestRows(std::vector<std::vector<int>>& mat, int k) {
-        std::multimap<int,int> bin_map;
-        std::vector<int> return_vec(0);
-        for(int i = 0; i < mat.size(); i++){
-            bin_map.insert(std::pair(std::accumulate(mat[i].begin(),mat[i].end(),0),i));
+    int numberOfSteps(int num) {
+
+        if (num != 0) {
+            num % 2 == 0 ? num = num / 2 : num = num - 1;
+            return 1 + numberOfSteps(num);
         }
-        auto itb = bin_map.begin();
-        for(int i = 0; i < k; i++,itb++){
-            return_vec.push_back(itb->second);
-        }
-        return return_vec;
+        return 0;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     Solution t;
-    std::vector<std::vector<int>> mat = {{1,1,0,0,0},
-                                         {1,1,1,1,0},
-                                         {1,0,0,0,0},
-                                         {1,1,0,0,0},
-                                         {1,1,1,1,1}};
-    std::vector<int> test = t.kWeakestRows(mat,3);
-    std::copy(test.begin(), test.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << t.numberOfSteps(14);
     return 0;
 }
