5d46eec59cc80ab0f0774194c8eabdcf166caf66
Igor <igoranchikkuzmin@yandex.ru>
2022-09-23 16:51:21 +0300

    Find Pivot Index

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 class Solution {
 public:
-    int numberOfSteps(int num) {
+    int pivotIndex(std::vector<int> &nums) {
 
-        if (num != 0) {
-            num % 2 == 0 ? num = num / 2 : num = num - 1;
-            return 1 + numberOfSteps(num);
+        int left_sum = 0;
+        int right_sum = std::accumulate(nums.begin(), nums.end(), 0);
+
+        for (std::size_t i = 0; i < nums.size(); i++) {
+            int current_val = nums[i];
+            right_sum -= current_val;
+            if (left_sum == right_sum) {
+                return i;
+            }
+            left_sum += current_val;
         }
-        return 0;
+        return -1;
     }
 };
 
 int main(int argc, const char *argv[]) {
     Solution t;
-    std::cout << t.numberOfSteps(14);
+    std::vector<int> nums = {-1, -1, -1, -1, -1, 1};
+    std::cout << t.pivotIndex(nums);
     return 0;
 }
