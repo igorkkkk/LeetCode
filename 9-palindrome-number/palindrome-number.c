806cd665ca150089a4db16a79052faec4a9ed1e2
Igor <68161226+igorktech@users.noreply.github.com>
2023-11-28 11:42:57 +0100

    Time: 12 ms (32.72%) | Memory: 6.7 MB (59.88%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,8 @@
+bool isPalindrome(int x) {
+    long long t = x, sum = 0;
+        while(t){
+            sum =t%10+sum*10;
+            t /= 10;
+        }
+        return x>=0 ? (sum==x) : false;
+}
