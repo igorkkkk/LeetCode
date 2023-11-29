954fc8fa7a4b65a5d42139ec18d4ebe6996a4e0e
Igor <68161226+igorktech@users.noreply.github.com>
2023-11-29 21:16:39 +0100

    Time: 0 ms (100.00%) | Memory: 6.7 MB (18.98%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.c
+++ 409-longest-palindrome/longest-palindrome.c
@@ -1,10 +1,12 @@
 int longestPalindrome(char* s) {
     int d[58] = {0};
-    int len = strlen(s);
+    int len = 0;
     int result = 0;
 
-    for (int i = 0; i < len; ++i)
+    for (int i = 0; s[i] != '\0'; ++i){
         d[s[i]-'A']++;
+        len++;
+    }
     
     for (int i = 0; i < 58; ++i)
         result += (d[i] / 2) * 2;
