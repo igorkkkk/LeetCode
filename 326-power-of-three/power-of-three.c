a44caeea41c3ea3b3c0ed955ec396d85661160be
Igor <68161226+igorktech@users.noreply.github.com>
2023-11-29 13:24:53 +0100

    Time: 20 ms (26.81%) | Memory: 7 MB (34.39%) - LeetSync

--- 326-power-of-three/power-of-three.c
+++ 326-power-of-three/power-of-three.c
@@ -1,3 +1,5 @@
 bool isPowerOfThree(int n) {
-    return (n > 0) && (1162261467 % n == 0);
+    // return (n > 0) && (1162261467 % n == 0);
+        double log = log10(n)/ log10(3);
+        return (roundf(log) == log) && n;
 }
