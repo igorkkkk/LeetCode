28457b744bdb9d54676bf4b29734f5d8ef1ab19c
Igor <68161226+igorktech@users.noreply.github.com>
2023-11-29 15:26:38 +0100

    Time: 6 ms (55.76%) | Memory: 6.7 MB (100.00%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ #define BUFFER_SIZE 1001
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    *returnSize = 0;
+
+    int map[BUFFER_SIZE] = {0};
+
+    int res_size =  (nums1Size < nums2Size) ? nums1Size : nums2Size;
+    int* res = (int*)malloc(sizeof(int)*res_size);
+
+    for (int i = 0; i < nums1Size; ++i)
+        map[nums1[i]]+=1; // set occurances
+    
+    for (int i = 0; i < nums2Size; ++i)
+        if (map[nums2[i]]){
+            res[(*returnSize)++] = nums2[i];
+            map[nums2[i]]--;
+        }
+    
+    return res;
+}
