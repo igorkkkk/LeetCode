aecba0578242fdd64a048c809675f5ac088e2d44
Igor <68161226+igorktech@users.noreply.github.com>
2023-11-30 10:44:36 +0100

    Time: 12 ms (27.19%) | Memory: 7 MB (13.60%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.c
@@ -0,0 +1,30 @@
+int comp (const int *i, const int *j);
+
+int thirdMax(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof (int), ( int(*) (const void *, const void *)) comp);
+    for(int i = 0; i < numsSize; i++){
+        printf("%d",nums[i]);
+    }
+    if (numsSize < 3)
+        return nums[0];
+
+    int count = 0;
+    for(int i = 0; i < numsSize-1; i++){
+        if(nums[i] != nums[i+1]){
+            count++;
+            if (count == 2)
+            return nums[i+1];
+        }
+    }
+    return nums[0];
+}
+
+int comp (const int *a, const int *b)
+{
+    if (*a > *b) 
+        return -1;
+    else if (*a < *b) 
+        return 1; 
+    else 
+        return 0; 
+}
