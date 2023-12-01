6fed08d0663fce8601024b3090081a58d61800e2
Igor <68161226+igorktech@users.noreply.github.com>
2023-12-01 09:23:15 +0100

    Time: 2 ms (49.66%) | Memory: 6.6 MB (40.33%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.c
@@ -0,0 +1,51 @@
+
+
+
+typedef struct {
+    int* arr;
+    int top;
+} MyStack;
+
+
+MyStack* myStackCreate() {
+    MyStack* pStack = (MyStack*)malloc(sizeof(MyStack));
+    pStack->arr = (int*)malloc(sizeof(int)*100);
+    pStack->top = -1;
+
+    return pStack;
+}
+
+void myStackPush(MyStack* obj, int x) {
+    obj->arr[++obj->top] = x;
+}
+
+int myStackPop(MyStack* obj) {
+    return obj->arr[obj->top--];
+}
+
+int myStackTop(MyStack* obj) {
+     return obj->arr[obj->top];
+}
+
+bool myStackEmpty(MyStack* obj) {
+    return obj->top == -1;
+}
+
+void myStackFree(MyStack* obj) {
+    free(obj->arr);
+    free(obj);
+}
+
+/**
+ * Your MyStack struct will be instantiated and called as such:
+ * MyStack* obj = myStackCreate();
+ * myStackPush(obj, x);
+ 
+ * int param_2 = myStackPop(obj);
+ 
+ * int param_3 = myStackTop(obj);
+ 
+ * bool param_4 = myStackEmpty(obj);
+ 
+ * myStackFree(obj);
+*/
