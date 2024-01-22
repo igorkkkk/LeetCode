58fdf1d8aed03ab708024bbb0533ed7db87544c8
Igor <68161226+igorktech@users.noreply.github.com>
2024-01-22 19:36:26 +0100

    Time: 4 ms (59.57%) | Memory: 5.9 MB (99.51%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.c
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.c
@@ -1,6 +1,7 @@
 #define SHIFT 1000
 #define MAX_SIZE 2001
 #define MAX_OCCUR 1001
+
 bool uniqueOccurrences(int* arr, int arrSize) {
     int hash[MAX_SIZE] = {0};
     int hash_occur[MAX_OCCUR] = {0};
