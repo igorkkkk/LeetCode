00d5994dfcdd6b1a8b7e4d0c7371757b2ed72e74
Igor <68161226+igorktech@users.noreply.github.com>
2024-08-22 16:25:44 +0200

    Time: 4 ms (56.97%) | Memory: 8.2 MB (19.85%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.c
+++ 205-isomorphic-strings/isomorphic-strings.c
@@ -1,13 +1,14 @@
 bool isIsomorphic(char* s, char* t) {
     char hash_s[128] = {0};
     char hash_t[128] = {0};
+    int i = 1;
     while (*s && *t) {
-        if (hash_s[*s] == NULL && hash_t[*t]==NULL)
-            hash_s[*s] = *t, hash_t[*t] = *s;
-        else
-            if (hash_s[*s] != *t || hash_t[*t] != *s)
-                return false;
-        s++, t++;
+        if (hash_s[*s] != hash_t[*t])
+            return false;
+        
+        hash_s[*s] = i;
+        hash_t[*t] = i;
+        s++, t++, i++;
     }
     return true;
 }
